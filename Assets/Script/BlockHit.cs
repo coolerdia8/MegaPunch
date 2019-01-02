31f96e9ceabd4ad9dd3d879b1ade66dae61b1ea8
K.Yutaka <kayser250tr@gmail.com>
2019-01-02 12:26:59 +0900

    Sound　途中

--- Assets/Script/BlockHit.cs
+++ Assets/Script/BlockHit.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BlockHit : MonoBehaviour
 {
     //オブジェクトと接触した瞬間に呼び出される
     void OnTriggerEnter(Collider other)
     {
-
+        HitSound.main.PlaySound(0);
         //攻撃した相手がEnemyの場合
         if (other.CompareTag("Block"))
         {
---
+++ Assets/Script/HitSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class HitSound : MonoBehaviour
+{
+    //音楽ファイル
+    [SerializeField] AudioClip[] audios;
+    //音を鳴らすコンポー
+    AudioSource audioSource;
+    //外部読み出し用
+    public static HitSound main;
+
+    void Start()
+    {
+        main = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    //鳴らす
+    public void PlaySound(int no)
+    {
+        audioSource.clip = audios[no];
+        audioSource.Play();
+    }
+}
